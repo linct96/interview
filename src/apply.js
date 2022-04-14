98ff45947c49335b1c270aa4bb8c1e6af228c057
linct96 <314220356@qq.com>
2022-04-14 18:07:36 +0800

    apply

--- src/apply.js
+++ src/apply.js
@@ -1,10 +1,30 @@
-const myApply = (ctx=window,...args) => {
-  const symbol = Symbol()
-  ctx[symbol] = this
-  const params = args[0] || []
-  const result = params.length ? context.fn(...params) : context.fn()
-  delete ctx[symbol]
+function myApply(context, arr) {
+  var ctx = Object(context) || window
+  ctx._fn = this
+  var result
+  if (arr) {
+    var args = []
+    for (let i = 0; i < arr.length; i++) {
+      args.push('arr[' + i + ']')
+    }
+    result = eval('context._fn(' + args.toString() + ')')
+  } else {
+    result = ctx._fn()
+  }
+  delete ctx._fn
 
   return result
 }
 
+Function.prototype.myApply = myApply
+
+const a = {
+  value: 3
+}
+
+function add(v1 = 0, v2 = 0) {
+  return this.value + v1 + v2
+}
+
+const result = add.myApply(a, [1, 2])
+console.log(result)
