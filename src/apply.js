658441b4fbc45c81bf5a1c40b257e4afebf658d8
linct96 <314220356@qq.com>
2022-04-15 11:22:27 +0800

    添加注释

--- src/apply.js
+++ src/apply.js
@@ -1,5 +1,8 @@
 function myApply(context, arr) {
+  // 将 context 包装为对象。
   var ctx = Object(context) || window
+  // 挂载函数到 context 上。
+  // 如果 _fn 已经存在，可以利用 hasOwnProperty 检测，保存之前的函数。
   ctx._fn = this
   var result
   if (arr) {
@@ -7,6 +10,7 @@ function myApply(context, arr) {
     for (let i = 0; i < arr.length; i++) {
       args.push('arr[' + i + ']')
     }
+    // 字符串拼接会自动调用 args 的 toString 方法。
     result = eval('context._fn(' + args + ')')
   } else {
     result = ctx._fn()
--- src/call.js
+++ src/call.js
@@ -1,10 +1,15 @@
 function myCall(context) {
+  // 将 context 包装为对象。
   var ctx = context ? Object(context) : window
+  // 挂载函数到 context 上。
+  // 如果 _fn 已经存在，可以利用 hasOwnProperty 检测，保存之前的函数。
   ctx._fn = this
   var args = []
+  // arguments[0] 为上下文，所以从 index = 1 开始
   for (var i = 1; i < arguments.length; i++) {
     args.push('arguments[' + i + ']')
   }
+  // 字符串拼接会自动调用 args 的 toString 方法。
   var result = eval('ctx._fn(' + args + ')')
   delete ctx._fn
 
