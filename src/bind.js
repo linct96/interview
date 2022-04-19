11777ff414e8801acc0ae0bb7be8e3c0cc2414c8
linct96 <314220356@qq.com>
2022-04-19 17:46:31 +0800

    bind

--- src/bind.js
+++ src/bind.js
@@ -1,22 +1,35 @@
-const myBind = (ctx) => {
-  const self = this
-  const args = [...arguments].slice(1)
+function myBind(ctx) {
+  var fn = this
+  // 校验是否被函数调用
+  if (typeof fn !== 'function') throw new Error()
+  // 获取剩余参数
+  var args = Array.prototype.slice.call(arguments, 1)
 
-  const nullFn = function(){}
-  const bindFn = function(){
-    const realCtx = this instanceof self ? this : ctx
-    return _self.apply(realCtx, args.concat(Array.prototype.slice.call(arguments)))
+  var noop = function () {}
+
+  var bindFn = function () {
+    // 区分被 new 时，this 的指向
+    var realCtx = this instanceof bindFn ? this : ctx
+    var bindFnArgs = Array.prototype.slice.call(arguments)
+    return fn.apply(realCtx, args.concat(bindFnArgs))
   }
 
-  // 维护原型关系
-  if (this.prototype) {
-    nullFn.prototype = this.prototype;
-  }
+  // 将原函数的原型指向绑定函数的原型
+  noop.prototype = this.prototype
+  bindFn.prototype = new noop()
 
-  bindFn.prototype = new fnNop();
-
+  return bindFn
 }
-const obj = {}
-const foo = function(){}
 
-const bindFoo = foo.bind(obj)
+Function.prototype.myBind = myBind
+
+const obj = {
+  name: 'obj'
+}
+const foo = function (a, b) {
+  this.name = this.name ? this.name : ''
+  console.log(this.name + a + b)
+}
+
+const bindFoo = foo.myBind(obj)
+bindFoo('c', 'd')
